3a3695b5b6a0275434a700d3a15a9293c7958189
AlexCatch <alex@alexcatchpoledev.me>
2019-05-04 14:54:39 +0100

    Initial Commit

---
+++ Katachi/Katachi.h
@@ -0,0 +1,19 @@
+//
+//  Katachi.h
+//  Katachi
+//
+//  Created by Alex Catchpole on 04/05/2019.
+//  Copyright © 2019 Alex Catchpole. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Katachi.
+FOUNDATION_EXPORT double KatachiVersionNumber;
+
+//! Project version string for Katachi.
+FOUNDATION_EXPORT const unsigned char KatachiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Katachi/PublicHeader.h>
+
+
